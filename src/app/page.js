604b4a9761ecd0dc353e6c38d52993d37871bf2f
Huu Le <huu.le@timo.vn>
2024-03-26 11:06:01 +0700

    Add favicon

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,14 @@ import Image from "next/image";
 import { Button } from "antd";
 import VietQrForm from "./qr_setting/component/VietQrForm";
 
+export const metadata = {
+  title: "Create your bank qr code",
+  description: "Support create bank qr code free",
+  icons: {
+    icon: 'https://app2.timo.vn/cxpassets/frontend/images/pfm/tomi_rock_scissors_paper/avt.jpeg',
+  },
+};
+
 export default function Home() {
   return <VietQrForm></VietQrForm>
 }
--- src/app/qr_setting/page.js
+++ src/app/qr_setting/page.js
@@ -8,6 +8,9 @@ import VietQrForm from './component/VietQrForm';
 export const metadata = {
     title: "Create your bank qr code",
     description: "Support create bank qr code free",
+    icons: {
+        icon: 'https://app2.timo.vn/cxpassets/frontend/images/pfm/tomi_rock_scissors_paper/avt.jpeg',
+    },
 };
 
 
