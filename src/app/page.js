2da063034954ac301955de268269943249b8da3d
Huu Le <huu.le@timo.vn>
2024-03-26 12:52:28 +0700

    Update social share link

--- src/app/page.js
+++ src/app/page.js
@@ -8,6 +8,11 @@ export const metadata = {
   icons: {
     icon: 'https://app2.timo.vn/cxpassets/frontend/images/pfm/tomi_rock_scissors_paper/avt.jpeg',
   },
+  openGraph: {
+    title: "Create your bank qr code",
+    description: "Support create bank qr code free",
+    images: 'https://app2.timo.vn/cxpassets/frontend/images/pfm/tomi_rock_scissors_paper/avt.jpeg',
+  },
 };
 
 export default function Home() {
