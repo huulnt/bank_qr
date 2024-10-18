0f3a870f9a63011c1de1e79c65ef6c990d7ae760
Huu Le <huu.le@timo.vn>
2024-10-18 17:15:31 +0700

    feat: handle navigator

--- src/app/shortcut/component/ShortCutController.js
+++ src/app/shortcut/component/ShortCutController.js
@@ -2,6 +2,8 @@
 
 import { useSearchParams } from 'next/navigation';
 
+import { useEffect } from 'react';
+
 const ShortCutController = () => {
 
   const searchParams = useSearchParams()
@@ -10,10 +12,16 @@ const ShortCutController = () => {
   const appIcon = searchParams.get('app_icon');
   const redirectRrl = searchParams.get('redirect_url');
   
+  useEffect(() => {
+    if (redirectRrl) {
+      document.location = redirectRrl;
+    }
+    return () => {
+      
+    };
+  }, [redirectRrl]);
 
 
-  window.open(redirectRrl)
-  
   return <></>
 }
 
