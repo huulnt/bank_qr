aa15020bdecb03b78312fb0a9a7054da6b4972b9
Huu Le <huu.le@timo.vn>
2024-10-24 15:04:29 +0700

    fix: vib not have data

--- src/app/bank/vib/supper-card-diem-chap-nhan-the/component/Table.js
+++ src/app/bank/vib/supper-card-diem-chap-nhan-the/component/Table.js
@@ -4,14 +4,13 @@ import { useState } from 'react';
 import { Input, Table, Tooltip, Typography } from "antd";
 
 import vibCard from '/public/local_data/vib_supper_card.json';
-import { queryPosts } from './api/vib';
 const { Text } = Typography;
 
 
 
 const TableComponent = () => {
 
-  const [source, setSource] = useState([]);
+  const [source, setSource] = useState(vibCard);
   const regex = /[^\w\s']/g;
 
   const columns = [
@@ -49,27 +48,25 @@ const TableComponent = () => {
   ];
 
   const handleChangeValue = (value) => {
-    // const search = vibCard.filter(item => {
-    //   const name = item.name.toLowerCase();
-    //   const type = item.type.toLowerCase();
-    //   const note = item.note.toLowerCase();
-    //   const text = value.toLowerCase();
+    const search = vibCard.filter(item => {
+      const name = item.name.toLowerCase();
+      const type = item.type.toLowerCase();
+      const note = item.note.toLowerCase();
+      const text = value.toLowerCase();
 
-    //   return name.includes(text) || type.includes(text) || note.includes(text);
-    // });
-    const result = queryPosts();
-
-    console.log('result: ',result);
-    
+      return name.includes(text) || type.includes(text) || note.includes(text);
+    });
     setSource(search);
   }
 
   return <div className="flex justify-center flex-col items-center  w-full">
-     <div className="mb-4 w-1/2">
+    <div className="mb-4 w-1/2">
       <Input placeholder="Tìm kiếm" size="large" onChange={(data) => {
+        console.log("value: ", data.target.value)
         handleChangeValue(data.target.value)
       }} />
     </div>
+
     <div className="w-full over">
       <Table columns={columns} dataSource={source} scroll={{ x: 500 }}/>
     </div>
